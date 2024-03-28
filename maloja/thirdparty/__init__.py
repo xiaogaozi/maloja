cb1682f61bbe119e4dc97469346ff31eb87242f8
Changjian Gao <gaochangjian@gmail.com>
2024-03-28 17:46:56 +0800

    Format code

--- maloja/thirdparty/__init__.py
+++ maloja/thirdparty/__init__.py
@@ -19,19 +19,16 @@ from .. import database
 from ..__pkginfo__ import USER_AGENT
 from ..pkg_global.conf import malojaconfig
 
-services = {
-    "proxyscrobble":[],
-    "import":[],
-    "metadata":[]
-}
-
+services = {"proxyscrobble": [], "import": [], "metadata": []}
 
 
 class InvalidResponse(Exception):
     """Invalid Response from Third Party"""
 
+
 class RateLimitExceeded(Exception):
     """Rate Limit exceeded"""
+
 
 # have a limited number of worker threads so we don't completely hog the cpu with
 # these requests. they are mostly network bound, so python will happily open up 200 new
@@ -40,19 +37,22 @@ thirdpartylock = BoundedSemaphore(4)
 
 
 def import_scrobbles(identifier):
-    for service in services['import']:
+    for service in services["import"]:
         if service.identifier == identifier:
             return service.import_scrobbles()
     return False
 
-def proxy_scrobble_all(artists,title,timestamp):
+
+def proxy_scrobble_all(artists, title, timestamp):
     for service in services["proxyscrobble"]:
-        service.scrobble(artists,title,timestamp)
+        service.scrobble(artists, title, timestamp)
+
 
 def get_image_track_all(track):
     with thirdpartylock:
         for service in services["metadata"]:
-            if "track" not in service.metadata["enabled_entity_types"]: continue
+            if "track" not in service.metadata["enabled_entity_types"]:
+                continue
             try:
                 res = service.get_image_track(track)
                 if res:
@@ -62,10 +62,13 @@ def get_image_track_all(track):
                     log(f"Could not get track image for {track} from {service.name}")
             except Exception as e:
                 log(f"Error getting track image from {service.name}: {e.__doc__}")
+
+
 def get_image_artist_all(artist):
     with thirdpartylock:
         for service in services["metadata"]:
-            if "artist" not in service.metadata["enabled_entity_types"]: continue
+            if "artist" not in service.metadata["enabled_entity_types"]:
+                continue
             try:
                 res = service.get_image_artist(artist)
                 if res:
@@ -75,10 +78,13 @@ def get_image_artist_all(artist):
                     log(f"Could not get artist image for {artist} from {service.name}")
             except Exception as e:
                 log(f"Error getting artist image from {service.name}: {e.__doc__}")
+
+
 def get_image_album_all(album):
     with thirdpartylock:
         for service in services["metadata"]:
-            if "album" not in service.metadata["enabled_entity_types"]: continue
+            if "album" not in service.metadata["enabled_entity_types"]:
+                continue
             try:
                 res = service.get_image_album(album)
                 if res:
@@ -93,8 +99,10 @@ def get_image_album_all(album):
 class GenericInterface:
     def active_proxyscrobble(self):
         return False
+
     def active_import(self):
         return False
+
     def active_metadata(self):
         return False
 
@@ -114,23 +122,23 @@ class GenericInterface:
         t = Thread(target=self.authorize)
         t.daemon = True
         t.start()
-        #self.authorize()
+        # self.authorize()
 
     # this makes sure that of every class we define, we immediately create an
     # instance (de facto singleton). then each instance checks if the requirements
     # are met to use that service in each particular role and registers as such
-    def __init_subclass__(cls,abstract=False):
+    def __init_subclass__(cls, abstract=False):
         if not abstract:
             s = cls()
             if s.active_proxyscrobble():
                 services["proxyscrobble"].append(s)
-                #log(cls.name + " registered as proxy scrobble target")
+                # log(cls.name + " registered as proxy scrobble target")
             if s.active_import():
                 services["import"].append(s)
-                #log(cls.name + " registered as scrobble import source")
+                # log(cls.name + " registered as scrobble import source")
             if s.active_metadata():
                 services["metadata"].append(s)
-                #log(cls.name + " registered as metadata provider")
+                # log(cls.name + " registered as metadata provider")
 
     def authorize(self):
         return True
@@ -138,67 +146,60 @@ class GenericInterface:
 
 
 # proxy scrobbler
-class ProxyScrobbleInterface(GenericInterface,abstract=True):
-
-    proxyscrobble = {
-        "required_settings":[],
-        "activated_setting":None
-    }
+class ProxyScrobbleInterface(GenericInterface, abstract=True):
+    proxyscrobble = {"required_settings": [], "activated_setting": None}
 
     # service provides this role only if the setting is active AND all
     # necessary auth settings exist
     def active_proxyscrobble(self):
         return (
-            all(self.settings[key] not in [None,"ASK",False] for key in self.proxyscrobble["required_settings"]) and
-            malojaconfig[self.proxyscrobble["activated_setting"]]
+            all(
+                self.settings[key] not in [None, "ASK", False]
+                for key in self.proxyscrobble["required_settings"]
+            )
+            and malojaconfig[self.proxyscrobble["activated_setting"]]
         )
 
-    def scrobble(self,artists,title,timestamp):
+    def scrobble(self, artists, title, timestamp):
         response = requests.post(
             url=self.proxyscrobble["scrobbleurl"],
-            data=self.proxyscrobble_postdata(artists,title,timestamp),
-            headers={
-                "User-Agent":self.useragent
-            }
+            data=self.proxyscrobble_postdata(artists, title, timestamp),
+            headers={"User-Agent": self.useragent},
         )
         if self.proxyscrobble["response_type"] == "xml":
             responsedata = response.text
             data = ElementTree.fromstring(responsedata)
             return self.proxyscrobble_parse_response(data)
 
-# scrobble import
-class ImportInterface(GenericInterface,abstract=True):
 
-    scrobbleimport = {
-        "required_settings":[],
-        "activated_setting":None
-    }
+# scrobble import
+class ImportInterface(GenericInterface, abstract=True):
+    scrobbleimport = {"required_settings": [], "activated_setting": None}
 
     # service provides this role only if the setting is active AND all
     # necessary auth settings exist
     def active_import(self):
         return (
-            all(self.settings[key] not in [None,"ASK",False] for key in self.scrobbleimport["required_settings"])
-            #and malojaconfig[self.scrobbleimport["activated_setting"]]
+            all(
+                self.settings[key] not in [None, "ASK", False]
+                for key in self.scrobbleimport["required_settings"]
+            )
+            # and malojaconfig[self.scrobbleimport["activated_setting"]]
             # registering as import source doesnt do anything on its own, so no need for a setting
         )
 
     def import_scrobbles(self):
         for scrobble in self.get_remote_scrobbles():
             database.incoming_scrobble(
-                artists=scrobble['artists'],
-                title=scrobble['title'],
-                time=scrobble['time']
+                artists=scrobble["artists"],
+                title=scrobble["title"],
+                time=scrobble["time"],
             )
 
 
 # metadata
-class MetadataInterface(GenericInterface,abstract=True):
-
-    metadata = {
-        "required_settings":[],
-        "activated_setting":None
-    }
+class MetadataInterface(GenericInterface, abstract=True):
+    metadata = {"required_settings": [], "activated_setting": None}
 
     delay = 0
 
@@ -206,19 +207,22 @@ class MetadataInterface(GenericInterface,abstract=True):
     # necessary auth settings exist
     def active_metadata(self):
         return (
-            all(self.settings[key] not in [None,"ASK",False] for key in self.metadata["required_settings"]) and
-            self.identifier in malojaconfig["METADATA_PROVIDERS"]
+            all(
+                self.settings[key] not in [None, "ASK", False]
+                for key in self.metadata["required_settings"]
+            )
+            and self.identifier in malojaconfig["METADATA_PROVIDERS"]
         )
 
-    def get_image_track(self,track):
+    def get_image_track(self, track):
         artists, title = track
         artiststring = urllib.parse.quote(", ".join(artists))
         titlestring = urllib.parse.quote(title)
         response = requests.get(
-            self.metadata["trackurl"].format(artist=artiststring,title=titlestring,**self.settings),
-            headers={
-                "User-Agent":self.useragent
-            }
+            self.metadata["trackurl"].format(
+                artist=artiststring, title=titlestring, **self.settings
+            ),
+            headers={"User-Agent": self.useragent},
         )
 
         if self.metadata["response_type"] == "json":
@@ -226,42 +230,36 @@ class MetadataInterface(GenericInterface,abstract=True):
             imgurl = self.metadata_parse_response_track(data)
         else:
             imgurl = None
-        if imgurl is not None: imgurl = self.postprocess_url(imgurl)
+        if imgurl is not None:
+            imgurl = self.postprocess_url(imgurl)
         time.sleep(self.delay)
         return imgurl
 
-    def get_image_artist(self,artist):
+    def get_image_artist(self, artist):
         artiststring = urllib.parse.quote(artist)
-        url = self.metadata["artisturl"].format(artist=artiststring,**self.settings)
-        log(f"Get artist image of \"{artist}\" from {url}")
-        response = requests.get(
-            url,
-            headers={
-                "User-Agent":self.useragent
-            }
-        )
+        url = self.metadata["artisturl"].format(artist=artiststring, **self.settings)
+        log(f'Get artist image of "{artist}" from {url}')
+        response = requests.get(url, headers={"User-Agent": self.useragent})
 
         if self.metadata["response_type"] == "json":
             data = response.json()
             imgurl = self.metadata_parse_response_artist(data)
         else:
             imgurl = None
-        if imgurl is not None: imgurl = self.postprocess_url(imgurl)
+        if imgurl is not None:
+            imgurl = self.postprocess_url(imgurl)
         time.sleep(self.delay)
         return imgurl
 
-    def get_image_album(self,album):
+    def get_image_album(self, album):
         artists, title = album
         artiststring = urllib.parse.quote(", ".join(artists or []))
         titlestring = urllib.parse.quote(title)
-        url = self.metadata["albumurl"].format(artist=artiststring,title=titlestring,**self.settings)
-        log(f"Get album image of \"{title}\" from {url}")
-        response = requests.get(
-            url,
-            headers={
-                "User-Agent":self.useragent
-            }
+        url = self.metadata["albumurl"].format(
+            artist=artiststring, title=titlestring, **self.settings
         )
+        log(f'Get album image of "{title}" from {url}')
+        response = requests.get(url, headers={"User-Agent": self.useragent})
 
         if self.metadata["response_type"] == "json":
             data = response.json()
@@ -269,27 +267,32 @@ class MetadataInterface(GenericInterface,abstract=True):
                 # Match album item by artist name
                 imgurl = None
                 for item in self._parse_response("response_parse_album_items", data):
-                    artist_name = self._parse_response("response_parse_album_item_artist_name", item)
+                    artist_name = self._parse_response(
+                        "response_parse_album_item_artist_name", item
+                    )
                     if artist_name in artists:
-                        imgurl = self._parse_response("response_parse_album_item_imgurl", item)
+                        imgurl = self._parse_response(
+                            "response_parse_album_item_imgurl", item
+                        )
                         break
             else:
                 imgurl = self.metadata_parse_response_album(data)
         else:
             imgurl = None
-        if imgurl is not None: imgurl = self.postprocess_url(imgurl)
+        if imgurl is not None:
+            imgurl = self.postprocess_url(imgurl)
         time.sleep(self.delay)
         return imgurl
 
     # default function to parse response by descending down nodes
     # override if more complicated
-    def metadata_parse_response_artist(self,data):
+    def metadata_parse_response_artist(self, data):
         return self._parse_response("response_parse_tree_artist", data)
 
-    def metadata_parse_response_track(self,data):
+    def metadata_parse_response_track(self, data):
         return self._parse_response("response_parse_tree_track", data)
 
-    def metadata_parse_response_album(self,data):
+    def metadata_parse_response_album(self, data):
         return self._parse_response("response_parse_tree_album", data)
 
     def _parse_response(self, resp, data):
@@ -298,45 +301,40 @@ class MetadataInterface(GenericInterface,abstract=True):
             try:
                 res = res[node]
             except Exception:
-                handleresult = self.handle_json_result_error(data) #allow the handler to throw custom exceptions
+                handleresult = self.handle_json_result_error(
+                    data
+                )  # allow the handler to throw custom exceptions
                 # it can also return True to indicate that this is not an error, but simply an instance of 'this api doesnt have any info'
                 if handleresult is True:
                     return None
-                #throw the generic error if the handler refused to do anything
+                # throw the generic error if the handler refused to do anything
                 raise InvalidResponse()
         return res
 
-    def postprocess_url(self,url):
-        url = url.replace("http:","https:",1)
+    def postprocess_url(self, url):
+        url = url.replace("http:", "https:", 1)
         return url
 
-    def handle_json_result_error(self,result):
+    def handle_json_result_error(self, result):
         raise InvalidResponse()
-
-
 
 
 ### useful stuff
 
+
 def utf(st):
     return st.encode(encoding="UTF-8")
+
+
 def b64(inp):
     return base64.b64encode(inp)
 
 
-
 ### actually create everything
 
-__all__ = [
-    "lastfm",
-    "spotify",
-    "musicbrainz",
-    "audiodb",
-    "deezer",
-    "maloja"
-]
+__all__ = ["lastfm", "spotify", "musicbrainz", "audiodb", "deezer", "maloja"]
 from . import *
 
 services["metadata"].sort(
-    key=lambda provider : malojaconfig["METADATA_PROVIDERS"].index(provider.identifier)
+    key=lambda provider: malojaconfig["METADATA_PROVIDERS"].index(provider.identifier)
 )
